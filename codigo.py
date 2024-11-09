7b44a2cab5ebf15d9bb1491ebf5da089d22d1e31
JosakaX <Josaka.Py@gmail.com>
2024-11-09 03:15:51 -0400

    sumando apellido a la variable

--- codigo.py
+++ codigo.py
@@ -1,5 +1,6 @@
 nombre = "Josaka"
 apellido = "X"
+edad = 33
 
-saludo = "Hola "+ nombre + " " + apellido + " ¿Como estas?"
+saludo = "Hola "+ nombre + " " + apellido + " ¿Como estas?" + "Tu edad" + edad
 print(saludo)
